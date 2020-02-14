7c902b06fd46ee54fb1326fa727e8bddd5ea0e18
Chaima-DB <chimox751@gmail.com>
2020-02-14 15:00:19 +0100

    hi

--- src/main/java/com/fstg/projet/ws/rest/MagasinRest.java
+++ src/main/java/com/fstg/projet/ws/rest/MagasinRest.java
@@ -1,5 +1,6 @@
-package com.fstg.projet.ws;
+package com.fstg.projet.ws.rest;
 
+import com.fstg.projet.ws.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
