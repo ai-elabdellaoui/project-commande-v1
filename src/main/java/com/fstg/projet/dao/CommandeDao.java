5685fd606fee026daa8e0e45d0e6917b66295089
HP <HP@DESKTOP-8ND2F9E>
2020-02-14 14:53:04 +0000

    add

--- src/main/java/com/fstg/projet/dao/CommandeDao.java
+++ src/main/java/com/fstg/projet/dao/CommandeDao.java
@@ -5,9 +5,10 @@ import java.io.Serializable;
 import org.springframework.stereotype.Repository;
 
 import com.fstg.projet.bean.Commande;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CommandeDao  extends Serializable{
+public interface CommandeDao  extends JpaRepository<Commande, Long>{
 	public Commande findByRef( String ref);
 
 }
