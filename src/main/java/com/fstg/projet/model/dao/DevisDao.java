b8618c70e33075f45390fd5e757737f58a41d46b
DELL <DELL@DESKTOP-OT04KG3>
2020-02-14 10:20:18 +0100

    repository

--- src/main/java/com/fstg/projet/model/dao/DevisDao.java
+++ src/main/java/com/fstg/projet/model/dao/DevisDao.java
@@ -1,5 +1,11 @@
 package com.fstg.projet.model.dao;
 
-public interface DevisDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fstg.projet.bean.Devis;
+
+@Repository
+public interface DevisDao extends JpaRepository<Devis, Long> {
 
 }
