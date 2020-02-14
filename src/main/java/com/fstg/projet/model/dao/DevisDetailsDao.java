de3734def3ab93f40166228d523618e2b3572832
DELL <DELL@DESKTOP-OT04KG3>
2020-02-14 10:42:50 +0100

    Devis Detail Dao

---
+++ src/main/java/com/fstg/projet/model/dao/DevisDetailsDao.java
@@ -0,0 +1,15 @@
+package com.fstg.projet.model.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fstg.projet.bean.Devis;
+import com.fstg.projet.bean.DevisDetail;
+import com.fstg.projet.bean.Produit;
+
+@Repository
+public interface DevisDetailsDao  extends JpaRepository<DevisDetail,Long>{
+
+	
+	public DevisDao findbyDevieEtProduit(Produit produit,Devis devis);
+}
